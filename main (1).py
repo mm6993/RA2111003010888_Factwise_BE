d0776a17a1070fcf66db7a4f79c6df67cc8126ec
Taruni Mamidipaka <104311908+mm6993@users.noreply.github.com>
2024-07-09 15:37:35 +0530

    question 1.py

--- main (1).py
+++ main (1).py
@@ -1,29 +1,29 @@
-def number_to_words(n):
+def numToWords(n):
     ones=["","one","two","three","four","five","six","seven","eight","nine"]
     teens=["","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
     tens=["","ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
     if n==1000:
         return "onethousand"
     words=""
-    hundreds_digit=n//100
-    tens_digit=(n%100)//10
-    ones_digit=n%10
-    if hundreds_digit>0:
-        words+=ones[hundreds_digit]+"hundred"
-        if tens_digit>0 or ones_digit>0:
+    hundredsDigit=n//100
+    tensDigit=(n%100)//10
+    onesDigit=n%10
+    if hundredsDigit>0:
+        words+=ones[hundredsDigit]+"hundred"
+        if tensDigit>0 or onesDigit>0:
             words+="and"
-    if tens_digit==1 and ones_digit>0:
-        words+=teens[ones_digit]
+    if tensDigit==1 and onesDigit>0:
+        words+=teens[onesDigit]
     else:
-        words+=tens[tens_digit]
-        words+=ones[ones_digit]
+        words+=tens[tensDigit]
+        words+=ones[onesDigit]
     return words
-def count_letters_in_numbers():
-    total_letters=0
+def letInNumbers():
+    totalLetters=0
     for i in range(1,1001):
-        words=number_to_words(i)
-        total_letters+=len(words)
-    return total_letters 
+        words=numToWords(i)
+        totalLetters+=len(words)
+    return totalLetters 
 if __name__=="__main__":
-    total_letters=count_letters_in_numbers()
-    print("Total letters used from 1 to 1000(inclusive):",total_letters)
+    totalLetters=letInNumbers()
+    print("total numbers from 1 to 1000:",totalLetters)
